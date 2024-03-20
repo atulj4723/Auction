4f41ce9d4cd90c92b1ed1a81984704a3e642c832
Atul Mohan Jadhav <139961613+atulj4723@users.noreply.github.com>
2024-03-20 13:07:02 +0000

    Add validation function

--- class.h
+++ class.h
@@ -2,7 +2,7 @@
 #include<fstream>
 using namespace std;
 class member{
-char ch1=0;
+int signin_choice;
 public:
 void signin()
 {
@@ -13,26 +13,64 @@ void signin()
   cout<<"3.Admin"<<endl;
   cout<<"4.Exit"<<endl;
   cout<<"Enter your choice:-";
-  cin>>ch1;
-  switch(ch1){
-    case '1':
+  cin>>signin_choice;
+  switch(signin_choice){
+    case 1:
         cout<<"Bidder"<<endl;
         break;
-    case '2':
+    case 2:
         cout<<"Seller"<<endl;
         break;
-    case '3':
+    case 3:
         cout<<"Admin"<<endl;
         break;
-    case '4':
+    case 4:
         cout<<"Exit"<<endl;
         break;
     default :
       cout<<"Invalid choice "<<endl;
        break;
 
-          }}while (ch1!='4');
+          }}while (signin_choice!=4);
 }
 void login ()
 {}
+int int_validate(int input,int lower,int upper)
+		{
+			int flag=0;
+			do{
+				if( (input>=lower) && (input<=upper) )
+				{
+					flag=1;
+					return input;
+				}
+				else{
+					cout<<"\n\n Wrong Input !!! \n\nEnter Correct Value : - ";
+					cin.clear();
+					fflush(stdin);
+					cin>>input;
+          flag=0;
+				}
+			}while(flag!=1);
+  return input;
+		}
+		long long int long_long_int_validate(long long int input,long long int lower, long long int upper)
+		{
+			int flag=0;
+			do{
+				if( (input>=lower) && (input<=upper) )
+				{
+					flag=1;
+					return input;
+				}
+				else{
+					cout<<"\n\n Wrong Input !!! \n\nEnter Correct Value : - ";
+					cin.clear();
+					fflush(stdin);
+					cin>>input;
+          flag=0;
+				}
+			}while(flag!=1);
+    return input;
+    }
 };
